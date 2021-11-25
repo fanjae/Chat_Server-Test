7f4c8ca1ed39995844b2519f562baa41776d147c
fanjaekim <hjjang45@gmail.com>
2021-11-25 23:46:45 +0900

    비동기 IO 모델 서버 구현

---
+++ Chat_Server2_Notification IO/Chat_Server2_Notification IO/AsynNotiEchoServ_win.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <winsock2.h>
+
+#define BUF_SIZE 100
+
+void CompressSockets(SOCKET hSockArr[], int idx, int total);
+void CompressEvents(WSAEVENT hEventArr[], int idx, int total);
+void ErrorHandling(char msg);
+
+int main(int argc, char *argv[])
+{
+	WSADATA wsaData;
+	SOCKET hServSock, hClntSock;
+	SOCKADDR_IN servAdr, clntAdr;
+
+	SOCKET hSockArr[WSA_MAXIMUM_WAIT_EVENTS];
+	WSAEVENT hEvent[WSA_MAXIMUM_WAIT_EVENTS];
