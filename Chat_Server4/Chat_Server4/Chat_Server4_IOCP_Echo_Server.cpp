40821f3d9e4370fe65ee8fbf258390126ad3fea6
fanjaekim <hjjang45@gmail.com>
2021-12-06 23:45:52 +0900

    Chat Server4 IOCP 기반 Echo Server

--- Chat_Server4/Chat_Server4/Chat_Server4_IOCP_Echo_Server.cpp
+++ Chat_Server4/Chat_Server4/Chat_Server4_IOCP_Echo_Server.cpp
@@ -48,5 +48,34 @@ int main(int argc, char *argv[])
 		_beginthreadex(NULL, 0, EchoThreadMain, (LPVOID)hComPort, 0, NULL);
 	}
 
+	hServSock = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
+	memset(&servAdr, 0, sizeof(servAdr));
+	servAdr.sin_family = AF_INET;
+	servAdr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servAdr.sin_port = htons(atoi(argv[1]));
 
+	bind(hServSock, (SOCKADDR*)&servAdr, sizeof(servAdr));
+	listen(hServSock, 5);
+
+	while (1)
+	{
+		SOCKET hClntSock;
+		SOCKADDR_IN clntAdr;
+		int addrLen = sizeof(clntAdr);
+
+		hClntSock = accept(hServSock, (SOCKADDR*)&clntAdr, &addrLen);
+		handleInfo = (LPPER_HANDLE_DATA)malloc(sizeof(PER_HANDLE_DATA));
+		handleInfo->hClntSock = hClntSock;
+		memcpy(&(handleInfo->clntAdr), &clntAdr, addrLen);
+
+		CreateIoCompletionPort((HANDLE)hClntSock, hComPort, (DWORD)handleInfo, 0);
+
+		ioInfo = (LPPER_IO_DATA)malloc(sizeof(PER_IO_DATA));
+		memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
+		ioInfo->wsaBuf.len = BUF_SIZE;
+		ioInfo->wsaBuf.buf = ioInfo->buffer;
+		ioInfo->rwMode = READ;
+		WSARecv(handleInfo->hClntSock, &(ioInfo->wsaBuf), 1, &recvBytes, &flags, &(ioInfo->overlapped), NULL);
+	}
+	return 0;
 }
