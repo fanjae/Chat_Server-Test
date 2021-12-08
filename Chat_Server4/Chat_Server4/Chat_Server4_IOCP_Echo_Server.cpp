a64fc84dddad9fb2b06e21005becb277f61a879c
fanjaekim <hjjang45@gmail.com>
2021-12-08 23:00:47 +0900

    IOCP_Echo Server 구현중 - EchoThreadMain() 내용 추가

--- Chat_Server4/Chat_Server4/Chat_Server4_IOCP_Echo_Server.cpp
+++ Chat_Server4/Chat_Server4/Chat_Server4_IOCP_Echo_Server.cpp
@@ -109,10 +109,36 @@ DWORD WINAPI EchoThreadMain(LPVOID pComPort)
 	LPPER_HANDLE_DATA handleInfo;
 	DWORD flags = 0;
 
-	/*
+	
 	while(1)
 	{
 		GetQueuedCompletionStatus(hComPort, &bytesTrans, (LPDWORD)&handleInfo, (LPOVERLAPPED*)&ioInfo, INFINITE);
+		sock = handleInfo->hClntSock;
+
+		if (ioInfo->rwMode == READ)
+		{
+			puts("message received!");
+			if (bytesTrans == 0) // EOF Àü¼Û ½Ã
+			{
+				closesocket(sock);
+				free(handleInfo);
+				free(ioInfo);
+				continue;
+			}
+
+			memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
+			ioInfo->wsaBuf.len = bytesTrans;
+			ioInfo->rwMode = WRITE;
+			WSASend(sock, &(ioInfo->wsabuf), 1, NULL, 0, &(ioInfo->overlapped), NULL);
+
+			ioInfo = (LPPER_IO_DATA)malloc(sizeof(PER_IO_DATA));
+			memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
+			ioInfo->wsaBuf.len = BUF_SIZE;
+			ioInfo->wsabuf.buf = ioInfo->buffer;
+			ioInfo->rwMode = READ;
+			WSARecv(sock, &(ioInfo->wsaBuf), 1, NULL, &flags, &(ioInfo->overlapped), NULL);
+		}
+
 	}
-	*/
+	
 }
